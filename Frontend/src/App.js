8c58b1da8a659ceb0c8997c7aab5867a4f9b3e4d
Ishan Makharia <ishanmakharia@Ishans-MacBook-Air.local>
2023-03-27 01:28:02 +0530

    first commit

---
+++ Frontend/src/App.js
@@ -0,0 +1,55 @@
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Profile from "./pages/profile/Profile";  
+import Register from "./pages/register/Register";
+import Messenger from "./pages/messenger/Messenger";
+/*import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";*/
+
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
+
+
+function App() {
+
+  const {user} = useContext(AuthContext)
+
+  return (
+    /*
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          {user ? <Home/> : <Register/>}
+        </Route>
+        <Route path="/login">
+          {user ? <Redirect to="/"/> : <Login/>}
+        </Route>
+        <Route path="/register">
+        {user ? <Redirect to="/"/> : <Register/>}
+        </Route>
+        <Route path="/profile/:username">
+          <Profile/>
+        </Route>
+        
+      </Switch>
+    </Router>
+    */
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={user ? <Home/> : <Register/>}/>
+        <Route path="/login" element={user ? <Navigate to="/"/> : <Login/>}/>
+        <Route path="/register" element={user ? <Navigate to="/"/> : <Register/>}/>
+        <Route path="/messenger" element={!user ? <Navigate to="/"/> : <Messenger/>}/>
+        <Route path="/profile/:username" element={<Profile/>}/>
+      </Routes>
+    </BrowserRouter>
+
+  );
+}
+
+export default App;
---
+++ Frontend/src/context/AuthContext.js
@@ -0,0 +1,36 @@
+import { createContext, useEffect, useReducer } from "react";
+import AuthReducer from "./AuthReducer";
+
+const INITIAL_STATE = {
+    // user: {//null,
+    //     _id:"63435fa80101f3cf9c42ac93",
+    //     username:"john",
+    //     email:"john@gmail.com",
+    //     profilePic:"",
+    //     coverPic:"",
+    //     followers:[],
+    //     followings:[],
+    //     isAdmin:false,
+    // },
+    user:JSON.parse(localStorage.getItem("user")) || null,
+    isFetching: false,
+    error: false
+};
+
+export const AuthContext = createContext(INITIAL_STATE);
+
+export const AuthContextProvider = ({children})=>{
+    const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
+
+    useEffect(()=>{
+        localStorage.setItem("user", JSON.stringify(state.user))
+      },[state.user]);
+
+    return (
+        <AuthContext.Provider 
+            value={{user: state.user, isFetching:state.isFetching, error:state.error, dispatch}}
+        >
+            {children}
+        </AuthContext.Provider>
+    )
+}
